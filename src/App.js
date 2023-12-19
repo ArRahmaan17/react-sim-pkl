babf08aa8fd865d9417b93c65209f3ba732a1c95
RahmaanM17 <ardrah17@gmail.com>
2023-12-19 16:55:26 +0700

    refactor: sim pkl application (frontend)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [listOfUsers, setUsers] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3001/users").then((response) => {
+      setUsers(response.data.data)
+    })
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ol>
+        {listOfUsers.map((value, key) => {
+          return <li>{value.phone_number} / {value.email}</li>
+        })}
+      </ol>
     </div>
   );
 }
