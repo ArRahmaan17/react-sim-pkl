3f60b91073638e2b67af8086024b2beadae210a4
RahmaanM17 <ardrah17@gmail.com>
2024-07-17 15:52:47 +0700

    feat: must login before chating

--- src/helpers/context.js
+++ src/helpers/context.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 import { createContext } from 'react';
 import { jwtDecode } from 'jwt-decode';
 import moment from 'moment';
-let env = require("../config/config.json");
+let env = require("../config/config.json")
 
 export const ThemeContext = createContext('light');
 export const AuthContext = createContext(null);
